1ad4db3af3e54342fa50501d26faffb08a826f21
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-07-12 21:57:07 +0530

    Add book reducer and action creators

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,1 +1,22 @@
-console.log('redux books');
+const ADD_BOOK = 'ADDBOOK';
+const REMOVE_BOOK = 'REMOVEBOOK';
+const INITIAL_STATE = [];
+
+export default bookReducer = (state = INITIAL_STATE, action) => {
+    const { type, book } = action;
+    switch (type) {
+        case ADD_BOOK: return [...state, book];
+        case REMOVE_BOOK: return state.filter(b => b.id !== book.id);
+        default: return state;
+    }
+}
+
+export const addBook = book => ({
+    type: ADD_BOOK,
+    book
+});
+
+export const removeBook = book => ({
+    type: REMOVE_BOOK,
+    book
+});
